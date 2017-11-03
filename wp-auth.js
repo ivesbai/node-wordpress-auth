d0d180d9fcdd15fdda636de079238534b0ba168d
baii <ives.bai@autodesk.com>
2017-11-03 16:10:57 +0800

    change mysql-native to mysql2

--- wp-auth.js
+++ wp-auth.js
@@ -28,8 +28,14 @@ function WP_Auth( wpurl, logged_in_key, logged_in_salt,
 	this.cookiename = 'wordpress_logged_in_' + md5.digest( 'hex' );
 	this.salt = logged_in_key + logged_in_salt;
 
-	this.db = require( 'mysql-native' ).createTCPClient( mysql_host );
-	this.db.auth( mysql_db, mysql_user, mysql_pass );
+	// this.db = require( 'mysql-native' ).createTCPClient( mysql_host );
+	// this.db.auth( mysql_db, mysql_user, mysql_pass );
+	this.db = require('mysql2').createConnection({
+		host: mysql_host,
+		user: mysql_user,
+		password: mysql_pass,
+		database: mysql_db
+	});
 	this.table_prefix = wp_table_prefix;
 
 	this.known_hashes = {};
@@ -60,87 +66,87 @@ WP_Auth.prototype.checkAuth = function( req ) {
 	return new Valid_Auth( data, this );
 };
 
-WP_Auth.prototype.getUserMeta = function( id, key, callback ) {
-	if ( !( id in this.meta_cache_timeout ) )
-		this.meta_cache_timeout[id] = {};
+// WP_Auth.prototype.getUserMeta = function( id, key, callback ) {
+// 	if ( !( id in this.meta_cache_timeout ) )
+// 		this.meta_cache_timeout[id] = {};
 
-	if ( key in this.meta_cache_timeout[id] && this.meta_cache_timeout[id][key] < +new Date ) {
-		delete this.meta_cache[id][key];
-		delete this.meta_cache_timeout[id][key];
-	}
+// 	if ( key in this.meta_cache_timeout[id] && this.meta_cache_timeout[id][key] < +new Date ) {
+// 		delete this.meta_cache[id][key];
+// 		delete this.meta_cache_timeout[id][key];
+// 	}
 
-	if ( id in this.meta_cache && key in this.meta_cache[id] ) {
-		callback( this.meta_cache[id][key] );
-		return;
-	}
+// 	if ( id in this.meta_cache && key in this.meta_cache[id] ) {
+// 		callback( this.meta_cache[id][key] );
+// 		return;
+// 	}
 
-	var self = this;
-	this.db.query( 'select meta_value from ' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and user_id = ' + parseInt( id ) ).on( 'row', function( data ) {
-		if ( !( id in self.meta_cache ) )
-			self.meta_cache[id] = {};
-		try {
-			self.meta_cache[id][key] = phpjs.unserialize( data.meta_value );
-		} catch( ex ) {
-			self.meta_cache[id][key] = data.meta_value;
-		}
-	} ).on( 'end', function() {
-		if ( !( id in self.meta_cache ) )
-			self.meta_cache[id] = {};
-		if ( !( key in self.meta_cache[id] ) )
-			self.meta_cache[id][key] = null;
-		self.meta_cache_timeout[id][key] = +new Date + self.timeout;
-		callback( self.meta_cache[id][key] );
-	} );
-};
+// 	var self = this;
+// 	this.db.query( 'select meta_value from ' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and user_id = ' + parseInt( id ) ).on( 'row', function( data ) {
+// 		if ( !( id in self.meta_cache ) )
+// 			self.meta_cache[id] = {};
+// 		try {
+// 			self.meta_cache[id][key] = phpjs.unserialize( data.meta_value );
+// 		} catch( ex ) {
+// 			self.meta_cache[id][key] = data.meta_value;
+// 		}
+// 	} ).on( 'end', function() {
+// 		if ( !( id in self.meta_cache ) )
+// 			self.meta_cache[id] = {};
+// 		if ( !( key in self.meta_cache[id] ) )
+// 			self.meta_cache[id][key] = null;
+// 		self.meta_cache_timeout[id][key] = +new Date + self.timeout;
+// 		callback( self.meta_cache[id][key] );
+// 	} );
+// };
 
-WP_Auth.prototype.setUserMeta = function( id, key, value ) {
-	if ( !( id in this.meta_cache_timeout ) )
-		this.meta_cache_timeout[id] = {};
+// WP_Auth.prototype.setUserMeta = function( id, key, value ) {
+// 	if ( !( id in this.meta_cache_timeout ) )
+// 		this.meta_cache_timeout[id] = {};
 
-	this.meta_cache[id][key] = value;
-	this.meta_cache_timeout[id][key] = +new Date + this.timeout;
+// 	this.meta_cache[id][key] = value;
+// 	this.meta_cache_timeout[id][key] = +new Date + this.timeout;
 
-	var sanitized_value = sanitizeValue( value );
+// 	var sanitized_value = sanitizeValue( value );
 
-	var self = this;
-	this.db.query( 'delete from' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and user_id = ' + parseInt( id ) );
-	this.db.query( 'insert into' + this.table_prefix + 'usermeta (meta_key, user_id, meta_value) VALUES(\'' + sanitizeValue( key ) + '\', ' + parseInt( id ) + ', \'' + sanitized_value + '\')' );
-};
+// 	var self = this;
+// 	this.db.query( 'delete from' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and user_id = ' + parseInt( id ) );
+// 	this.db.query( 'insert into' + this.table_prefix + 'usermeta (meta_key, user_id, meta_value) VALUES(\'' + sanitizeValue( key ) + '\', ' + parseInt( id ) + ', \'' + sanitized_value + '\')' );
+// };
 
-WP_Auth.prototype.reverseUserMeta = function( key, value, callback ) {
-	for ( var id in this.meta_cache ) {
-		if ( key in this.meta_cache[id] && this.meta_cache[id][key] == value ) {
-			callback( id );
-			return;
-		}
-	}
+// WP_Auth.prototype.reverseUserMeta = function( key, value, callback ) {
+// 	for ( var id in this.meta_cache ) {
+// 		if ( key in this.meta_cache[id] && this.meta_cache[id][key] == value ) {
+// 			callback( id );
+// 			return;
+// 		}
+// 	}
 
-	var id = null;
+// 	var id = null;
 
-	var self = this;
-	this.db.query( 'select user_id from ' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and meta_value = \'' + sanitizeValue( key ) + '\'' ).on( 'row', function( data ) {
-		id = data.user_id;
-		if ( !( id in self.meta_cache ) )
-			self.meta_cache[id] = {};
-		if ( !( id in self.meta_cache_timeout ) )
-			self.meta_cache_timeout[id] = {};
-		self.meta_cache[id][key] = value;
-		self.meta_cache_timeout[id][key] = +new Date + this.timeout;
-	} ).on( 'end', function() {
-		callback( id );
-	} );
-};
+// 	var self = this;
+// 	this.db.query( 'select user_id from ' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and meta_value = \'' + sanitizeValue( key ) + '\'' ).on( 'row', function( data ) {
+// 		id = data.user_id;
+// 		if ( !( id in self.meta_cache ) )
+// 			self.meta_cache[id] = {};
+// 		if ( !( id in self.meta_cache_timeout ) )
+// 			self.meta_cache_timeout[id] = {};
+// 		self.meta_cache[id][key] = value;
+// 		self.meta_cache_timeout[id][key] = +new Date + this.timeout;
+// 	} ).on( 'end', function() {
+// 		callback( id );
+// 	} );
+// };
 
-WP_Auth.prototype.getContributors = function( callback ) {
-	var self = this;
-    var users = [];
-	// this.db.query( 'select ID as user_id, user_login from ' + this.table_prefix + 'user' ).on( 'row', function( data ) {});
-	this.db.query( 'select user_id, user_login from ' + this.table_prefix + 'usermeta as meta left join ' + this.table_prefix + 'users as user on user.ID = meta.user_id where meta_key = \'wp_user_level\' and meta_value = \'1\'' ).on( 'row', function( data ) {
-        users.push( data );
-    }).on( 'end', function() {
-        callback( users );
-    });
-};
+// WP_Auth.prototype.getContributors = function( callback ) {
+// 	var self = this;
+//     var users = [];
+// 	// this.db.query( 'select ID as user_id, user_login from ' + this.table_prefix + 'user' ).on( 'row', function( data ) {});
+// 	this.db.query( 'select user_id, user_login from ' + this.table_prefix + 'usermeta as meta left join ' + this.table_prefix + 'users as user on user.ID = meta.user_id where meta_key = \'wp_user_level\' and meta_value = \'1\'' ).on( 'row', function( data ) {
+//         users.push( data );
+//     }).on( 'end', function() {
+//         callback( users );
+//     });
+// };
 
 exports.create = function( wpurl, logged_in_key, logged_in_salt,
 				mysql_host, mysql_user, mysql_pass, mysql_db,
@@ -193,13 +199,26 @@ function Valid_Auth( data, auth ) {
 
 
 	var found = false;
-	auth.db.query( 'select ID, user_login, user_pass, user_email, display_name from ' + auth.table_prefix + 'users where user_login = \'' + user_login.replace( /(\'|\\)/g, '\\$1' ) + '\'' ).on( 'row', function( data ) {
-		found = true;
-		auth.known_hashes[user_login] = {frag: data.user_pass.substr( 8, 4 ), id: data.ID, data: {id: data.ID, login: data.user_login, email: data.user_email, display_name: data.display_name}};
-		auth.known_hashes_timeout[user_login] = +new Date + auth.timeout;
-	} ).on( 'end', function() {
-		if ( !found ) {
+	// auth.db.query( 'select ID, user_login, user_pass, user_email, display_name from ' + auth.table_prefix + 'users where user_login = \'' + user_login.replace( /(\'|\\)/g, '\\$1' ) + '\'' ).on( 'row', function( data ) {
+	// 	found = true;
+	// 	auth.known_hashes[user_login] = {frag: data.user_pass.substr( 8, 4 ), id: data.ID, data: {id: data.ID, login: data.user_login, email: data.user_email, display_name: data.display_name}};
+	// 	auth.known_hashes_timeout[user_login] = +new Date + auth.timeout;
+	// } ).on( 'end', function() {
+	// 	if ( !found ) {
+	// 		auth.known_hashes[user_login] = {frag: '__fail__', id: 0, data: {}};
+	// 		auth.known_hashes_timeout[user_login] = +new Date + auth.timeout;
+	// 	}
+	// 	parse( auth.known_hashes[user_login].frag, auth.known_hashes[user_login].id, auth.known_hashes[user_login].data );
+	// } );
+
+	auth.db.query( 'select ID, user_login, user_pass, user_email, display_name from ' + auth.table_prefix + 'users where user_login = \'' + user_login.replace( /(\'|\\)/g, '\\$1' ) + '\'' , function( error, results, fields ) {
+		if (error || results.length === 0) {
 			auth.known_hashes[user_login] = {frag: '__fail__', id: 0, data: {}};
+			auth.known_hashes_timeout[user_login] = +new Date + auth.timeout;
+		} else {
+			found = true;
+			var data = results[0];
+			auth.known_hashes[user_login] = {frag: data.user_pass.substr( 8, 4 ), id: data.ID, data: {id: data.ID, login: data.user_login, email: data.user_email, display_name: data.display_name}};
 			auth.known_hashes_timeout[user_login] = +new Date + auth.timeout;
 		}
 		parse( auth.known_hashes[user_login].frag, auth.known_hashes[user_login].id, auth.known_hashes[user_login].data );
